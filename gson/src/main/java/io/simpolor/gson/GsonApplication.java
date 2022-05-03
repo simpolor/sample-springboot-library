5b333d96ccfdb72bffa878937f2aa6ad0e84177a
sy.park <sy.park@powderroom.co.kr>
2022-05-03 18:33:56 +0900

    .

---
+++ gson/src/main/java/io/simpolor/gson/GsonApplication.java
@@ -0,0 +1,13 @@
+package io.simpolor.gson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GsonApplication.class, args);
+    }
+
+}
---
+++ objectmapper/src/test/java/io/simpolor/objectmapper/ObjectmapperApplicationTests.java
@@ -0,0 +1,13 @@
+package io.simpolor.objectmapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObjectmapperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
